d69feda92cea2e7d3cea100e9ea806b0c8b094af
Klesley Silva de Oliveira <klesley.oliveira@hotmail.com>
2026-06-02 17:49:03 -0300

    Beaning CustomerEntityRepository and adding HAL Explorer

--- src/main/java/com/dio/marketplace/registration/infrastructure/persistence/repository/CustomerEntityRepository.java
+++ src/main/java/com/dio/marketplace/registration/infrastructure/persistence/repository/CustomerEntityRepository.java
@@ -3,9 +3,12 @@ package com.dio.marketplace.registration.infrastructure.persistence.repository;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.dio.marketplace.registration.infrastructure.persistence.entity.Customer;
 
-public interface CustomerEntityRepository extends CrudRepository<Customer, UUID> {
+@RepositoryRestResource
+public interface CustomerEntityRepository extends PagingAndSortingRepository<Customer, UUID>, CrudRepository<Customer, UUID> {
 
 }
